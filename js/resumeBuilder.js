140b86fc0982c176ae9a9ea2b237d7c1d102716f
Anne Gatchell <anne@orbotix.com>
2016-08-24 17:05:20 -0600

    Add resume JSON

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,100 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+var bio = {
+  "name" : "Anne Gatchell",
+  "role" : "Web Engineer",
+  "contacts" : {
+    "email" : "annegatchell@gmail.com",
+    "twitter" : "@aisforanne",
+    "mobile" : "303-548-6335",
+    "github" : "annegatchell",
+    "location" : "Boulder, CO"
+  },
+  "biopic" : "images/biopic.jpeg",
+  "welcomeMessage": "Hello, my name is Anne. I love to paint but I also program for money.",
+  "skills": ["ruby", "rails", "JavaScript", "AWS", "Oil Painting", "Party Dancing"]
+}
+
+var work = {
+  "jobs" : [
+    {
+      "position" : "Web Engineer",
+      "employer" : "Sphero",
+      "duration" : "1 year, 3 months",
+      "city" : "Boulder"
+    },
+    {
+      "position" : "Software Engineer",
+      "employer" : "Twitter",
+      "duration" : "1 year",
+      "city" : "Boulder"
+    },
+    {
+      "position" : "Software Engineer",
+      "employer" : "Gnip",
+      "duration" : "1 year",
+      "city" : "Boulder"
+    },
+    {
+      "position" : "Game Design Intern",
+      "employer" : "Dreamquest",
+      "duration" : "3 months",
+      "city" : "Lafayette"
+    },
+    {
+      "position" : "Software Engineer Intern",
+      "employer" : "Ricoh",
+      "duration" : "3 months",
+      "city" : "Boulder"
+    },
+    {
+      "position" : "Engineer I",
+      "employer" : "Ascent Solar",
+      "duration" : "2 years",
+      "city" : "Littleton"
+    }
+  ]
+}
+
+var projects = {
+  "projects" : [
+    {
+      "title": "Firmware API",
+      "dates" : "July-August 2016",
+      "description" : "Create an API for firmware engineers to upload new firmware files and for devices to acquire firmware files",
+      "images" : [
+        {
+          "url" : ""
+        },
+        {
+          "url" : ""
+        }
+      ]
+    },
+    {
+      "title" : "Activations Research",
+      "dates" : "January-March 2016",
+      "description" : "Determine if we are dropping activations or not",
+      "images" : [
+
+      ]
+    }
+  ]
+}
+
+var education = {
+  "schools" : [
+    {
+      "name" : "University of Colorado",
+      "degree" : "MS",
+      "major" : "Computer Science",
+      "city" : "Boulder",
+      "graduationYear" : 2013
+    },
+    {
+      "name" : "Johns Hopkins University",
+      "degree" : "BS",
+      "major" : "Chemical and Biomolecular Engineering",
+      "city" : "Baltimore",
+      "graduationYear" : 2008
+    }
+  ]
+}
